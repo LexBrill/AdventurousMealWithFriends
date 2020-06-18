8d186d9c40797896f11e3de82f034829446617f9
lexbrill <lexingtonbrill@gmail.com>
2020-06-17 20:33:23 -0700

    this is the worst

--- CallAPI.js
+++ CallAPI.js
@@ -1,5 +1,5 @@
 const eFile = require('child_process').execFile
-const process = execFile('python' , ['./findPlace.py', "Chinese", "New York", "low", "15"]);
+const process = eFile('python' , ['./findPlace.py', "Chinese", "New York", "low", "15"]);
 process.stdout.on('data' , data => {
         console.log(data.toString());
 });
