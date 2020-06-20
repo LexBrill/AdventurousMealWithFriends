fa32441423a3d3092645a8569e7b2b310a6c7359
lexbrill <lexingtonbrill@gmail.com>
2020-06-19 19:10:51 -0700

    Attempt with firebase python script call

--- CallAPI.js
+++ CallAPI.js
@@ -1,5 +1,5 @@
 const execFile = require('child_process').execFile
-const process = execFile('python' , ['./findPlace.py', "Chinese", "Seattle", "high", "15"]);
+const process = execFile('python' , ['./testScript.py', "Chinese", "Seattle", "high", "15"]);
 process.stdout.on('data' , data => {
         console.log(data.toString());
 });
--- functions/index.js
+++ functions/index.js
@@ -2,13 +2,26 @@ const functions = require('firebase-functions');
 const gcs = require('@google-cloud/storage');
 const os = require('os');
 const path = require('path');
+
+
+
+
+
+
+
 // const spawn = require('child-process-promise').spawn;
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+    const execFile = require('child_process').execFile
+    const process = execFile('python' , ['./testScript.py', "Chinese", "Seattle", "high", "15"]);
+    process.stdout.on('data' , data => {
+        console.log(data.toString());
+        response.send(data.toString());
+    });
+    // response.send("Hello from Firebase!");
 });
 
 // exports.onFileChange = functons.storage.object().onChange(event => {});
