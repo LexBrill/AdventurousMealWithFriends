f01d9b40f558714c9f88dadcc7baf6f6f6f1bad9
lexbrill <lexingtonbrill@gmail.com>
2020-06-20 21:45:10 -0700

    attempt at POST

--- CallAPI.js
+++ CallAPI.js
@@ -1,5 +1,23 @@
-const execFile = require('child_process').execFile
-const process = execFile('python' , ['./testScript.py', "Chinese", "Seattle", "high", "15"]);
-process.stdout.on('data' , data => {
-        console.log(data.toString());
-});
+// const execFile = require('child_process').execFile
+// const process = execFile('python' , ['./testScript.py', "Chinese", "Seattle", "high", "15"]);
+// process.stdout.on('data' , data => {
+//         console.log(data.toString());
+// });
+
+const getPosts = () => {
+    return fetch('https://us-central1-wherewegoing-7e97a.cloudfunctions.net/uploadFile')
+    .then(res => res.json())
+    .then(posts => console.log(posts))
+}
+
+const newPost = post => {
+    const options = {
+            method: 'POST',
+            body: JSON.stringify(post),
+            // headers: new Headers({
+            //     'Content-Type': 'application/json'
+            // })
+    }
+
+    return fetch('https://us-central1-wherewegoing-7e97a.cloudfunctions.net/uploadFile')
+}
